2e80974b87323af8394232260e73e6857bc2d8dc
jetwes <jetwes@users.noreply.github.com>
2020-09-17 12:52:12 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,8 @@
 namespace Jetwes\TelposAppraisalInterview\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Orchestra\Testbench\TestCase as Orchestra;
 use Jetwes\TelposAppraisalInterview\TelposAppraisalInterviewServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
